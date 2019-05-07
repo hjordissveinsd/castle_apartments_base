96ec70034b15b1383d4032bb836b2a4cc49bcfdd
Arnar Snaer <arnarsnaer98@gmail.com>
2019-05-07 14:47:46 +0000

    base.html getur farið á milli forsíðu og Browse

--- front/urls.py
+++ front/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name='front'),
+    path('front/', views.index, name='front')
     ]

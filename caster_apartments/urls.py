ffa97c2156b80b18ff68edbc0d0cddb255beb703
hjordissveinsd <hjordis16@ru.is>
2019-05-08 11:09:02 +0000

    Sell síða, byrjun

--- caster_apartments/urls.py
+++ caster_apartments/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', include('Profile.urls')),
     path('browse/', include('Browse.urls')),
-    path('settings/', include('Settings.urls'))
+    path('settings/', include('Settings.urls')),
+    path('sell/', include('Sell.urls'))
 ]

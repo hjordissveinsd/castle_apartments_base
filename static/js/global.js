664792af9960f57715458a52bc704799f0ce8485
eyglomaria <eyglo16@ru.is>
2019-05-09 15:35:50 +0000

    js file í base

---
+++ static/js/global.js
@@ -0,0 +1,11 @@
+
+var user=''
+
+console.log('hello')
+
+function logIn(email, password) {
+    var test = document.getElementById("eMail").value;
+    var
+    console.log(email)
+    console.log(password)
+}

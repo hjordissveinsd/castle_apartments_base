8a11086d40f9df9832dc94b3634f6079c34e8735
eyglomaria <eyglo16@ru.is>
2019-05-09 13:29:39 +0000

    log in og sign up síða

--- Messages/views.py
+++ Messages/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def messages(request):
-    return render(request, 'profile/messages.html')
+    return render(request, 'profile/createOrLogIn.html')

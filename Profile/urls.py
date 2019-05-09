c04d69af157f99222e46c09c204333513d911bc0
ragnar <ragnarr18@skel.ru.is>
2019-05-09 20:21:12 +0000

    vinna í login

--- Profile/urls.py
+++ Profile/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 from . import views
 
@@ -7,7 +8,8 @@ urlpatterns = [
     path('messages', views.messages, name="messages"),
     path('settings', views.settings, name="settings"),
     path('createOrLogIn', views.createOrLogIn, name="createOrLogIn"),
-    path('loggedIn', views.loggedIn, name="loggedIn"),
+    path('loggedIn', LoginView.as_view(template_name='Profile/loggedIn.html'), name='Login'),
+    #path('loggedIn', views.loggedIn, name="loggedIn"),
     path('notLoggedIn', views.notLoggedIn, name="notLoggedIn"),
     ##############raggi##############################
     path('register', views.register, name="register")

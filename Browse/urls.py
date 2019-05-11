ad55e303c31e2567affa9d7e1e3df7ee715db5d6
eyglomaria <eyglo16@ru.is>
2019-05-11 18:11:50 +0000

    BROWSE

--- Browse/urls.py
+++ Browse/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('',views.browse, name="browse"),
+    path('singleEstate', views.singleEstate, name="singleEstate"),
 ]
--- Browse/views.py
+++ Browse/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from Browse.models import Estate
+from Browse.models import ImageList
 
 # Create your views here.
 
@@ -8,5 +9,10 @@ from Browse.models import Estate
  #   return render(request, 'Browse/browse.html')
 
 def browse(request):
-    context = {'estates' : Estate.objects.all()}
+    context = {'estates' : Estate.objects.all().order_by('address'), 'imageList': ImageList.objects.all()}
     return render(request, 'Browse/browse.html', context )
+
+
+
+def singleEstate(request):
+    return render(request, 'Browse/single_estate.html.html')

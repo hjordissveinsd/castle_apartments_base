afb93519b5be7d987f7badf69d267ce5f37643dc
ragnar <ragnarr18@skel.ru.is>
2019-05-13 14:44:53 +0000

    vla

--- Browse/urls.py
+++ Browse/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('',views.browse, name="estate"),
     path('singleEstate', views.singleEstate, name="singleEstate"),
     path('create_estate', views.createEstate, name='create_estate'),
-    path('<int:id>', views.get_estate_by_id, name="estate_detail")
+    path('<int:id>', views.get_estate_by_id, name="estate_detail"),
+    #raggi að búa til kaup checkout
+    path('checkout', views.checkout, name="checkout")
 ]

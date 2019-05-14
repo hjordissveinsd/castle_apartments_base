4870d6c95aa5bf3876d6a8f559518443645322d4
Arnar Snaer <arnarsnaer98@gmail.com>
2019-05-14 14:03:50 +0000

    Laga villu

--- Browse/models.py
+++ Browse/models.py
@@ -5,19 +5,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Estate(models.Model):
     address = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    zipCode = models.IntegerField()
-    country = models.CharField(max_length=255)
     lotSize = models.IntegerField()
     houseSize = models.IntegerField()
-    estateType = models.CharField(max_length=255)
     bedNum = models.IntegerField()
     bathNum = models.IntegerField()
     price = models.IntegerField()
     desc = models.CharField(max_length=999)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.BooleanField()
-    buyerID = models.IntegerField()
     image = models.ImageField(upload_to='es_images/', blank=True)
     #bæta við, "if no pictures still post"
     #bæta við Estate type

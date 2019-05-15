3dff95953880f15a60315944ee5347f7fb95a182
Arnar Snaer <arnarsnaer98@gmail.com>
2019-05-15 16:24:22 +0000

    Litlir hlutir lagaðir

--- Sell/models.py
+++ Sell/models.py
@@ -1,18 +1,19 @@
-#from django.db import models
-#from Profile.models import User
+from django.db import models
+from Profile.models import User
 #from django.contrib.auth.models import User
 
 # Create your models here.
-#class Estate(models.Model):
- #   address = models.CharField(max_length=255)
- #   lotSize = models.IntegerField()
- #   houseSize = models.IntegerField()
- #   bedNum = models.IntegerField()
- #   bathNum = models.IntegerField()
- #   price = models.IntegerField()
- #   desc = models.CharField(max_length=999)
- #   owner = models.ForeignKey(User, on_delete=models.CASCADE)
- #   status = models.BooleanField()
- #   image = models.ImageField(upload_to='es_image/')
+class Estate(models.Model):
+    address = models.CharField(max_length=255)
+    lotSize = models.IntegerField()
+    houseSize = models.IntegerField()
+    bedNum = models.IntegerField()
+    bathNum = models.IntegerField()
+    price = models.IntegerField()
+    desc = models.CharField(max_length=999)
+    zip = models.IntegerField(max_length=4)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, initial={{User.pk}})
+    status = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='es_image/')
     #bæta við, "if no pictures still post"
     #bæta við Estate type

967904c31ce25de9fe891bebe3d2ffa719798e6c
hjordissveinsd <hjordis16@ru.is>
2019-05-16 19:15:36 +0000

    fokking filter virkni

---
+++ static/js/filter.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $('#filter-btn').on('click', function(e) {
+        e.preventDefault();
+        var searchText = $('#search-box').val();
+        $.ajax({
+            url:'/estate?zip_filter=' + searchText,
+            type: 'GET',
+            success: function(resp) {
+                var newHTML = resp.data.map(d => {
+                    return `<div class="thumbnail">
+                                <div class="imagelist" id="estateimg">
+                                    <img src="${'../media/'}${d.image}" id="img-pic" alt="EstateLogo"/>
+                                </div>
+
+                                <div class="caption" id="estatebox">
+                                    <h3>${d.address}</h3>
+                                    <h4>Price:${d.price} ISK</h4>
+                                    <div class="limit">
+                                        <p>${d.desc}</p>
+                                    </div>
+                                    <br>
+                                    <button class='btn' onclick="window.location.href = ${d.id};" id="dropdown-choice">Learn more</button>
+                                </div>
+                            </div>`
+                });
+                $('#flex-box').html(newHTML.join(''));
+                $('#search-box').val('');
+            },
+            error: function(xhr, status, error) {
+                console.error(error);
+            }
+        })
+    })
+});

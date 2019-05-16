184ef1074c458c679d44c6625c4f145bcfaf3e79
eyglomaria <eyglo16@ru.is>
2019-05-16 15:37:38 +0000

    safnajdsfng

--- Browse/urls.py
+++ Browse/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('singleEstate', views.singleEstate, name="singleEstate"),
     #path('create_estate', views.createEstate, name='create_estate'),
     path('<int:id>/', views.get_estate_by_id, name="estate_detail"),
-    path('<int:id>/payment_details', views.payment_details, name='payment_details'),
+    path('<int:id>/payment_details/', views.payment_details, name='payment_details'),
     path('<int:id>/payment_details/checkout', views.checkout, name='checkout')
 ]

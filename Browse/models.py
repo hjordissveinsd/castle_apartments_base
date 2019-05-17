c22b42aad43a82f2af315974fe494e99debd0b46
Arnar Snaer <arnarsnaer98@gmail.com>
2019-05-17 13:19:45 +0000

    Lagaði villu, fancy nöfn farin, learn more virkar á forsíðu

--- Browse/models.py
+++ Browse/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Estate(models.Model):
-    address = models.CharField(max_length=255, name="Address")
-    lotSize = models.IntegerField(name="Lot Size")
-    houseSize = models.IntegerField(name="House Size")
-    bedNum = models.IntegerField(name="Number of bedrooms")
-    bathNum = models.IntegerField(name="Number of bathrooms")
-    price = models.IntegerField(name="Price of estate")
-    desc = models.CharField(max_length=999, name="Description of estate")
-    city = models.CharField(max_length=100, name="City")
+    address = models.CharField(max_length=255)
+    lotSize = models.IntegerField()
+    houseSize = models.IntegerField()
+    bedNum = models.IntegerField()
+    bathNum = models.IntegerField()
+    price = models.IntegerField()
+    desc = models.CharField(max_length=999)
+    city = models.CharField(max_length=100)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    zip = models.IntegerField(name="Postal code")
+    zip = models.IntegerField()
     status = models.BooleanField()
-    image = models.ImageField(upload_to='es_images/', name="Image of estate")
+    image = models.ImageField(upload_to='es_images/')
     #bæta við, "if no pictures still post"
     #bæta við Estate type
